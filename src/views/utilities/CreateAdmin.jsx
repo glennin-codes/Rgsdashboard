af0c8faee16bf580bacd2b10e32af032d7ec7a32
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-09-12 21:54:24 +0300

    complte user regestration

--- src/views/utilities/CreateAdmin.jsx
+++ src/views/utilities/CreateAdmin.jsx
@@ -14,7 +14,7 @@ const initialState = {
   phone: '',
   photo: '',
   email: '',
-  password: '',
+  password: ''
 };
 
 export function CreateAdmin() {
@@ -23,146 +23,121 @@ export function CreateAdmin() {
   const [errorMessage, setErrorMessage] = useState('');
   const [openSuccessSnackbar, setOpenSuccessSnackbar] = useState(false);
   const [openErrorSnackbar, setOpenErrorSnackbar] = useState(false);
-  
+
   const handleInputChange = (event) => {
     const { name, value } = event.target;
     setFormData({ ...formData, [name]: value });
   };
 
   const handleGeneratePassword = () => {
-    const generatedPassword = generateRandomPassword(8); // Change 12 to your desired password length
+    const generatedPassword = generateRandomPassword(8); 
     setFormData({ ...formData, password: generatedPassword });
   };
 
   const handleSuccessSnackbarClose = () => {
     setOpenSuccessSnackbar(false);
   };
-  
+
   const handleErrorSnackbarClose = () => {
     setOpenErrorSnackbar(false);
   };
   const handleSubmit = async (event) => {
     event.preventDefault();
-  
-  const token=getDataFromLocalStorage('token');
-  
+
+    const token = getDataFromLocalStorage('token');
+
     // Create an Axios instance with the default headers
     const apiInstance = axios.create({
-      baseURL: 'https://jade-panda-robe.cyclic.app', 
+      baseURL: 'https://jade-panda-robe.cyclic.app',
       headers: {
-        'Authorization': `Bearer ${token}`,
-      },
+        Authorization: `Bearer ${token}`
+      }
     });
-  
-    try {
 
+    try {
       const response = await apiInstance.post('/api/admin', formData);
-  // Handle success
-     
-  setSuccessMessage(response.data.message);
-  setOpenSuccessSnackbar(true);
-      // Handle the API response as needed
-      console.log('API Response:', response.data);
-  
+      // Handle success
+
+      setSuccessMessage(response.data.message);
+      setOpenSuccessSnackbar(true);
+
       // Reset the form if needed
       setFormData(initialState);
     } catch (error) {
       setErrorMessage(error?.response?.data?.message);
       setOpenErrorSnackbar(true);
-      console.error('API Error:', error);
-
     }
   };
   return (
-
-  <MainCard title="Register an Admin">
-    <Container maxWidth="sm">
-      <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
-        
-        <form onSubmit={handleSubmit} style={{
-          marginTop:"20px"
-        }}>
-          <Grid container spacing={2}>
-            <Grid item xs={12}>
-              <TextField
-                label="Name"
-                name="name"
-                fullWidth
-                variant="outlined"
-                value={formData.name}
-                onChange={handleInputChange}
-              />
+    <MainCard title="Register an Admin">
+      <Container maxWidth="sm">
+        <Paper elevation={3} style={{ padding: '20px', marginTop: '20px' }}>
+          <form
+            onSubmit={handleSubmit}
+            style={{
+              marginTop: '20px'
+            }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField label="Name" name="name" fullWidth variant="outlined" value={formData.name} onChange={handleInputChange} />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Location"
+                  name="location"
+                  fullWidth
+                  variant="outlined"
+                  value={formData.location}
+                  onChange={handleInputChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField label="Phone" name="phone" fullWidth variant="outlined" value={formData.phone} onChange={handleInputChange} />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField label="Email" name="email" fullWidth variant="outlined" value={formData.email} onChange={handleInputChange} />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  label="Password"
+                  name="password"
+                  fullWidth
+                  variant="outlined"
+                  type="password"
+                  value={formData.password}
+                  onChange={handleInputChange}
+                  InputProps={{
+                    endAdornment: (
+                      <Button onClick={handleGeneratePassword} variant="contained">
+                        Generate
+                      </Button>
+                    )
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <PhotoUpload setFormData={setFormData} formData={formData} />
+              </Grid>
+              <Grid item xs={12}>
+                <AnimateButton>
+                  <Button type="submit" variant="contained" color="secondary" fullWidth>
+                    Register
+                  </Button>
+                </AnimateButton>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField
-                label="Location"
-                name="location"
-                fullWidth
-                variant="outlined"
-                value={formData.location}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                label="Phone"
-                name="phone"
-                fullWidth
-                variant="outlined"
-                value={formData.phone}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                label="Email"
-                name="email"
-                fullWidth
-                variant="outlined"
-                value={formData.email}
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                label="Password"
-                name="password"
-                fullWidth
-                variant="outlined"
-                type="password"
-                value={formData.password}
-                onChange={handleInputChange}
-                InputProps={{
-                  endAdornment: (
-                    <Button onClick={handleGeneratePassword} variant="contained">
-                      Generate
-                    </Button>
-                  ),
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <PhotoUpload setFormData={setFormData} formData={formData}/>
-            </Grid>
-            <Grid item xs={12}>
-              <AnimateButton>
-              <Button type="submit" variant="contained" color="secondary" fullWidth>
-                Register
-              </Button>
-              </AnimateButton>
-            </Grid>
-          </Grid>
-        </form>
-      </Paper>
-      <CustomSnackbar
-        openSuccessSnackbar={openSuccessSnackbar}
-        openErrorSnackbar={openErrorSnackbar}
-        successMessage={successMessage}
-        errorMessage={errorMessage}
-        handleSuccessSnackbarClose={handleSuccessSnackbarClose}
-        handleErrorSnackbarClose={handleErrorSnackbarClose}
-      />
-    </Container>
+          </form>
+        </Paper>
+        <CustomSnackbar
+          openSuccessSnackbar={openSuccessSnackbar}
+          openErrorSnackbar={openErrorSnackbar}
+          successMessage={successMessage}
+          errorMessage={errorMessage}
+          handleSuccessSnackbarClose={handleSuccessSnackbarClose}
+          handleErrorSnackbarClose={handleErrorSnackbarClose}
+        />
+      </Container>
     </MainCard>
   );
 }
