ce8661a7c49f2eb7dc19deb26883b2b519b8113c
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2023-09-28 03:58:36 +0300

    revamped the form

--- src/ui-component/FillForm/index.jsx
+++ src/ui-component/FillForm/index.jsx
@@ -1,23 +1,26 @@
-import * as React from 'react';
-import { TextField, Button, Grid, FormControl, InputLabel, Box } from '@mui/material';
+import React, { useState } from 'react';
+import './LandOwnershipForm.css'; // Import your external CSS for styling
 
 export default function LandOwnershipForm() {
-  const [values, setValues] = React.useState({
-    siro: '',
-    tarikh: '',
+  const [values, setValues] = useState({
+    No: '',
+    BollectarioNo:'',
+    Tirsi:"",
+    BolletaNo:"",
+    Taariikh: '',
     mudan: '',
-    baan: '',
-    xaafada: '',
-    xoog: '',
+    Sanadka: '',
+    Xaafadda: '',
+    vacant: '',
     mudMar: '',
-    field1: '',
-    field2: '',
-    field3: '',
-    field4: '',
-    field5: '',
-    field6: '',
-    field7: '',
-    field8: '',
+    X: '',
+    kunaYaal: '',
+    Degmada: '',
+    SoohdintiisuTahay: '',
+    Waqooyi: '',
+   Galbeed: '',
+    Bari: '',
+    kofuur: '',
     field9: '',
     field10: '',
     field11: '',
@@ -37,111 +40,216 @@ export default function LandOwnershipForm() {
 
   return (
     <form onSubmit={handleSubmit}>
-      <Grid container spacing={2}>
-        <Grid item xs={6}>
-          <FormControl fullWidth>
-            <InputLabel htmlFor="siro">Siro</InputLabel>
-            <TextField
-              id="siro"
-              name="siro"
-              value={values.siro}
+      <div className="form-container">
+        <div className="form-column">
+          <div className="form-field">
+            <label htmlFor="siro">No:</label>
+            <input
+              type="number"
+              id="No"
+              name="No"
+              value={values.No}
               onChange={handleChange}
             />
-          </FormControl>
-        </Grid>
-        <Grid item xs={6}>
-          <FormControl fullWidth>
-            <InputLabel htmlFor="tarikh">Tarikh</InputLabel>
-            <TextField
-              id="tarikh"
-              name="tarikh"
-              value={values.tarikh}
+          </div>
+          <div className="form-field">
+            <label htmlFor="BollectarioNo">Bollectario No:</label>
+            <input
+              type="number"
+              id="BollectarioNo"
+              name="BollectarioNo"
+              value={values.BollectarioNo}
               onChange={handleChange}
             />
-          </FormControl>
-        </Grid>
-        <Grid item xs={6}>
-          <FormControl fullWidth>
-            <InputLabel htmlFor="mudan">Mudan</InputLabel>
-            <TextField
-              id="mudan"
-              name="mudan"
-              value={values.mudan}
+          </div>
+          <div className="form-field">
+            <label htmlFor="Tirisi">Soo Gelidda Warqadda Tirsi:</label>
+            <input
+              type="text"
+              id="Tirsi"
+              name="Tirsi"
+              value={values.Tirsi}
               onChange={handleChange}
             />
-          </FormControl>
-        </Grid>
-        <Grid item xs={6}>
-          <FormControl fullWidth>
-            <InputLabel htmlFor="baan">Baan</InputLabel>
-            <TextField
-              id="baan"
-              name="baan"
-              value={values.baan}
+          </div>
+          <div className="form-field">
+            <label htmlFor="BolletaNo">Bolleta No:</label>
+            <input
+              type="text"
+              id="BolletaNo"
+              name="BolletaNo"
+              value={values.BolletaNo}
               onChange={handleChange}
             />
-          </FormControl>
-        </Grid>
-        <Grid item xs={6}>
-          <FormControl fullWidth>
-            <InputLabel htmlFor="xaafada">Xaafada</InputLabel>
-            <TextField
-              id="xaafada"
-              name="xaafada"
-              value={values.xaafada}
+          </div>
+          <div className="form-field">
+            <label htmlFor="Taariikh">Taariikh:</label>
+            <input
+              type="text"
+              id="Taariikh"
+              name="Taariikh"
+              value={values.Taariikh}
               onChange={handleChange}
             />
-          </FormControl>
-        </Grid>
-        <Grid item xs={6}>
-          <FormControl fullWidth>
-            <InputLabel htmlFor="xoog">Xoog</InputLabel>
-            <TextField
-              id="xoog"
-              name="xoog"
-              value={values.xoog}
+          </div>
+          <div className="form-field">
+            <label htmlFor="Sanadka">Sanadka:</label>
+            <input
+              type="text"
+              id="Sanadka"
+              name="Sanadka"
+              value={values.Sanadka}
               onChange={handleChange}
             />
-          </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-          {/* Title */}
-          Dowlad Goboleedka Koofur Galbeed - Waaxda Dhulka
-        </Grid>
-        <Grid item xs={12}>
-          <FormControl fullWidth>
-            <InputLabel htmlFor="mudMar">Mud/Mar</InputLabel>
-            <TextField
+          </div>
+        </div>
+        <div className="form-title">
+              WARQADDA LAHAANSHAHA DHULKA
+          </div>
+          <div className="form-field">
+            <label htmlFor="mudMar">Mud. / M ar.</label>
+            <input
+              type="text"
               id="mudMar"
               name="mudMar"
               value={values.mudMar}
               onChange={handleChange}
             />
-          </FormControl>
-        </Grid>
-        {/* Additional fields */}
-        {[...Array(12)].map((_, i) => (
-          <Grid item xs={(i + 1) % 4 === 0 ? 4 : 4} key={`field${i + 1}`}>
-            <FormControl fullWidth>
-              <InputLabel htmlFor={`field${i + 1}`}>Field {i + 1}</InputLabel>
-              <TextField
-                id={`field${i + 1}`}
-                name={`field${i + 1}`}
-                value={values[`field${i + 1}`]}
-                onChange={handleChange}
-              />
-            </FormControl>
-          </Grid>
-        ))}
-        {/* Submit button */}
-        <Grid item xs={12}>
-          <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-            <Button type="submit" variant="contained">
-              Submit
-            </Button>
-          </Box>
-        </Grid>
-      </Grid>
+          </div>
+          <div
+          className="form-title"
+          >
+            <h6>
+            ha / Marwada kor ku qoran waxaa loo oggolaaday inuu/inay dhisto cariish, Baraako ama Guri Dhagax ah Mudana 
+            </h6>
+          </div>
+     
+        <div className="form-column2">
+        <div className="form-field">
+            <label htmlFor="vacant" > </label>
+            <input
+              type="text"
+              id="vacant"
+              name="vacant"
+              value={values.vacant}
+              onChange={handleChange}
+            />
+          </div>
+        <div className="form-field">
+            <label htmlFor="X" > X :</label>
+            <input
+              type="text"
+              id="X"
+              name="X"
+              value={values.X}
+              onChange={handleChange}
+            />
+          </div>
+          <div
+          className='sub-name'
+          >
+            <p>
+            oo u uko dhiso / dhisto cad dhul ah oo la eg
+            </p>
+          </div>
+          <div className="form-field">
+            <label htmlFor="vacant" > </label>
+            <input
+              type="text"
+              id="vacant"
+              name="vacant"
+              value={values.vacant}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="X" > kuna yaal:</label>
+            <input
+              type="text"
+              id="kunaYaal"
+              name="kunaYaal"
+              value={values.kunaYaal}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="Dagmada" >Degmada:</label>
+            <input
+              type="text"
+              id="Degmada"
+              name="Degmada"
+              value={values.Degmada}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="Xaafadda" >Xaafadda:</label>
+            <input
+              type="text"
+              id="Xaafadda"
+              name="Xaafadda"
+              value={values.Xaafadda}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="SoohdintiisuTahay" >Soohdintiisu Tahay:</label>
+            <input
+              type="text"
+              id="SoohdintiisuTahay"
+              name="SoohdintiisuTahay"
+              value={values.SoohdintiisuTahay}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="Waqooyi" >X. Waqooyi:</label>
+            <input
+              type="text"
+              id="Waqooyi"
+              name="Waqooyi"
+              value={values.Waqooyi}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="Galbeed" >X. Galbeed:</label>
+            <input
+              type="text"
+              id="Galbeed"
+              name="Galbeed"
+              value={values.Galbeed}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="Bari" >X. Bari:</label>
+            <input
+              type="text"
+              id="Bari"
+              name="Bari"
+              value={values.Bari}
+              onChange={handleChange}
+            />
+          </div>
+          <div className="form-field">
+            <label htmlFor="kofuur" >iyo X.
+Koofur:</label>
+            <input
+              type="text"
+              id="kofuur"
+              name="kofuur"
+              value={values.kofuur}
+              onChange={handleChange}
+            />
+          </div>
+        
+          {/* Submit button */}
+          <div >
+            <button className="form-button" type="submit">Submit</button>
+          </div>
+        </div>
+      </div>
     </form>
   );
 }
